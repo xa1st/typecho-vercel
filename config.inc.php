3d86e4ca4ff371574c2760269122988f1a05b168
猫东东 <rakiy@qq.com>
2025-05-24 22:59:07 +0800

    更新配置

--- config.inc.php
+++ config.inc.php
@@ -8,7 +8,7 @@
  */
 
 /** 开启https */
-define('__TYPECHO_SECURE__',true);
+define('__TYPECHO_SECURE__', true);
 
 /** 定义根目录 */
 define('__TYPECHO_ROOT_DIR__', dirname(__FILE__));
@@ -23,9 +23,7 @@ define('__TYPECHO_THEME_DIR__', '/usr/themes');
 define('__TYPECHO_ADMIN_DIR__', '/admin/');
 
 /** 设置包含路径 */
-@set_include_path(get_include_path() . PATH_SEPARATOR .
-__TYPECHO_ROOT_DIR__ . '/var' . PATH_SEPARATOR .
-__TYPECHO_ROOT_DIR__ . __TYPECHO_PLUGIN_DIR__);
+@set_include_path(get_include_path() . PATH_SEPARATOR . __TYPECHO_ROOT_DIR__ . '/var' . PATH_SEPARATOR . __TYPECHO_ROOT_DIR__ . __TYPECHO_PLUGIN_DIR__);
 
 /** 载入API支持 */
 require_once 'Typecho/Common.php';
